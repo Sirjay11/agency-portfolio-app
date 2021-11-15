054197005519175e2080f0b8a8a78ef6c8e254f9
James-Leroy <jlanvja@gmail.com>
2021-11-15 18:28:10 -0500

    stage

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,29 @@
-const Navbar = () => {
- return (
-  <div>
-    navbar
-  </div>
- )
-}
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/">James Alexander</Link>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <Link href="/products/projects">PROJECTS</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/products/linkedin">LINKEDIN</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/products/contact">CONTACT</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/products/resume">RESUME</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/products/github">GITHUB</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
